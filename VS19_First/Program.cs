251fce65ac1c806cb9cf7737288abb09bee008bc
BEA <bandork@gmail.com>
2019-11-23 22:20:09 +1000

    :)
    Add project files.
    
    Add .gitignore and .gitattributes.

---
+++ VS19_First/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace VS19_First
+{
+	class Program
+	{
+
+		static Channel<string> YY = Channel.CreateUnbounded<string>();
+
+		static async Task Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			await funks.G(funks.Z());
+
+
+			//var consumeTask = Task.Run(async () =>
+			//{
+			//	await  UU();
+			//});
+
+			//await Task.Delay(2000);
+
+			//var produceTask = OO();
+
+			//await Task.WhenAll(produceTask);
+
+			//YY.Writer.Complete();
+
+			//await YY.Reader.Completion;
+
+			Console.WriteLine("Poop");
+
+			////await Task.WhenAll(w, o);
+
+			Console.ReadLine();
+		}
+
+		//public static async Task OO()
+		//{
+		//	await foreach (var p in funks.Z())
+		//	{
+		//		Console.WriteLine(p);
+		//		await YY.Writer.WriteAsync(p);
+		//	}
+		//}
+
+		//public static async Task UU()
+		//{
+		//	var hh = YY.Reader.ReadAllAsync();
+		//	funks.G(hh);
+		//	//while (await YY.Reader.WaitToReadAsync())
+		//	//{
+		//	//	Console.WriteLine("UUS");
+		//	//	await Task.Delay(3000);
+		//	//	var hh = YY.Reader.ReadAllAsync();
+
+		//	//	funks.G(hh);
+
+		//	//	Console.WriteLine("UUE");
+		//	//}
+		//}
+	}
+}
---
+++ VS19_First/funks.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS19_First
+{
+
+	public class MyObj
+	{
+		string val;
+
+		public MyObj(string val)
+		{
+			this.val = val;
+		}
+
+		public override string ToString()
+		{
+			return this.val;
+		}
+	}
+	static class funks
+	{
+
+
+		public static async IAsyncEnumerable<MyObj> Z()
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+
+				await Task.Delay(1000);
+
+				Console.WriteLine(i + " PROD");
+				yield return new MyObj(i.ToString());
+			}
+		}
+
+		internal static async Task G(IAsyncEnumerable<MyObj> hh)
+		{
+			List<MyObj> batch = new List<MyObj>();
+			await foreach (var zz in hh)
+			{
+				batch.Add(zz);
+				if (batch.Count > 5)
+				{
+					batch.ForEach(b => Console.WriteLine(b + " CONS"));
+					batch.Clear();
+				}
+			}
+			if (batch.Count > 0)
+			{
+				batch.ForEach(b => Console.WriteLine(b + " CONS"));
+				batch.Clear();
+			}
+		}
+	}
+}
